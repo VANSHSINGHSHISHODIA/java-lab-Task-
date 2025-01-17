58f9443651e1ccb1edea6349a03193c3f7b19fb7
VANSHSINGHSHISHODIA <168113021+VANSHSINGHSHISHODIA@users.noreply.github.com>
2025-01-17 15:15:50 +0530

    Details of Students code

---
+++ Task.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Task{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your Name:");
+        String name = scanner.nextLine();
+
+        
+
+
+        System.out.print("Enter your Address:");
+        String Address = scanner.nextLine();
+
+        System.out.print("Enter your Age:");
+        String Age = scanner.nextLine();
+
+        System.out.print("Enter your University Name:");
+        String College = scanner.nextLine();
+
+        //System.out.println(name);
+        //System.out.println(Address);
+        //System.out.println(Age);
+        //System.out.println(College);
+
+
+
+    }
+}
